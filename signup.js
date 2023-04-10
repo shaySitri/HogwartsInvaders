469bc611ce18972de8164fae03bdaa03baba8ad3
ShaySitri <sitri@post.bgu.ac.il>
2023-04-10 20:33:10 +0300

    user list updae adte submit

--- signup.js
+++ signup.js
@@ -1,5 +1,6 @@
 var users = [{username: 'p', password: 'testuser'}];
 i = 0
+subBut = document.getElementById("subForm");
 
 function isValid() {
     var errMsgs = document.getElementsByClassName("err");
@@ -14,48 +15,40 @@ function isValid() {
         if (details[i].value == "")
         {
             document.getElementById("errMsg").innerHTML = "Please fill in all fields!";
-            return false;
         }
     }
 
     if(details[1].value != details[2].value)
     {
         document.getElementById("errMsg1").innerHTML = "Please validate your password again!";
-        return false;
     }
 
     if(details[1].value.length < 8)
     {
         document.getElementById("errMsg1").innerHTML = "Your password is short.";
-        return false;
     }
 
     if(details[1].value.length < 7)
     {
         document.getElementById("errMsg1").innerHTML = "Your password is too short.";
-        return false;
     }
 
     if(((/[0-9]/i.test(details[3].value))))
     {
         document.getElementById("errMsg2").innerHTML = "Name field conatin only letters";
-        return false;
     }
     if(((/[0-9]/i.test(details[4].value))))
         {
         document.getElementById("errMsg3").innerHTML = "Name field conatin only letters";
-        return false;
     }
     if(!(/[a-z]/i.test(details[1].value)))
     {
         document.getElementById("errMsg1").innerHTML = "Your password weak. Add letters.";
-        return false;
     }
 
     if(!(/[0-9]/i.test(details[1].value)))
     {
         document.getElementById("errMsg1").innerHTML = "Your password weak. Add numbers.";
-        return false;
     }
 
     for(i = 0; i < users.length; i++)
@@ -63,16 +56,29 @@ function isValid() {
         if(details[0].value == users[i].username)
         {
             document.getElementById("errMsg4").innerHTML = "User already exist.";
-            return false;
         }
     }
-
     i = i+1
-    users[i] = 
-    {
-        username: details[0].value,
-        password: details[1].value
-    };
 
-    return true
+    addUser(details[0].value, details[1].value)
+    var details = document.getElementsByClassName("signUpField");
+    for (let i = 0; i < details.length; i++) {
+        details[i].value == ""
+    }
+
+    $( "#game" ).hide();
+    $( "#reg" ).hide();
+    $( "#welcome").show();
+}
+
+function addUser(name, pass)
+{
+    users[users.length] = 
+    {
+        username: name,
+        password: pass
+    }
+
+    alert("User Created!");
+
 }
