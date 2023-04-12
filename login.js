187bd51f506d3ad2f2219762dbf1fe8dde651c30
ShaySitri <sitri@post.bgu.ac.il>
2023-04-12 11:52:04 +0300

    login build

--- login.js
+++ login.js
@@ -1,4 +1,26 @@
 function login(uname, pass)
 {
-    
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == uname)
+        {
+            if (users[i].password != pass)
+            {
+                document.getElementById("login_errmsg").innerHTML = "Wrong Password.";
+                return false;
+            }
+            else
+            {
+                alert('Login Succeed!')
+                $( "#game" ).show();
+                $( "#welcome" ).hide();
+                $( "#reg" ).hide();
+                $( "#login" ).hide();
+                document.getElementById("span-username").innerHTML = uname;
+            }                
+
+        }
+    }
+    document.getElementById("login_errmsg").innerHTML = "User doesnt exist. Please register first.";
+    return false;
+
 }
