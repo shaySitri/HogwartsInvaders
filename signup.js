67651c812052699384b92cc4a703a4fc9f6dbbac
ShaySitri <sitri@post.bgu.ac.il>
2023-04-12 11:40:37 +0300

    register bug fixed

--- signup.js
+++ signup.js
@@ -1,12 +1,12 @@
 var users = [{username: 'p', password: 'testuser'}];
 i = 0
-subBut = document.getElementById("subForm");
+// subBut = document.getElementById("subForm");
 
 function isValid() {
     var errMsgs = document.getElementsByClassName("err");
     for(i=0; i<errMsgs.length; i++)
     {         
-    errMsgs[i].innerHTML = ""
+        errMsgs[i].innerHTML = ""
     }
 
     var details = document.getElementsByClassName("signUpField");
@@ -15,40 +15,49 @@ function isValid() {
         if (details[i].value == "")
         {
             document.getElementById("errMsg").innerHTML = "Please fill in all fields!";
+            return false;
+
         }
     }
 
     if(details[1].value != details[2].value)
     {
         document.getElementById("errMsg1").innerHTML = "Please validate your password again!";
+        return false;
     }
 
     if(details[1].value.length < 8)
     {
         document.getElementById("errMsg1").innerHTML = "Your password is short.";
+        return false;
     }
 
     if(details[1].value.length < 7)
     {
         document.getElementById("errMsg1").innerHTML = "Your password is too short.";
+        return false;
     }
 
     if(((/[0-9]/i.test(details[3].value))))
     {
         document.getElementById("errMsg2").innerHTML = "Name field conatin only letters";
+        return false;
     }
     if(((/[0-9]/i.test(details[4].value))))
         {
         document.getElementById("errMsg3").innerHTML = "Name field conatin only letters";
+        return false;
     }
     if(!(/[a-z]/i.test(details[1].value)))
     {
         document.getElementById("errMsg1").innerHTML = "Your password weak. Add letters.";
+        return false;
     }
 
     if(!(/[0-9]/i.test(details[1].value)))
     {
         document.getElementById("errMsg1").innerHTML = "Your password weak. Add numbers.";
+        return false;
     }
 
     for(i = 0; i < users.length; i++)
@@ -56,6 +65,7 @@ function isValid() {
         if(details[0].value == users[i].username)
         {
             document.getElementById("errMsg4").innerHTML = "User already exist.";
+            return false;
         }
     }
     i = i+1
@@ -69,6 +79,7 @@ function isValid() {
     $( "#game" ).hide();
     $( "#reg" ).hide();
     $( "#welcome").show();
+    return true;
 }
 
 function addUser(name, pass)
