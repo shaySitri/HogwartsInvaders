7da9e6aa7544af4daa111f22e6f4314e3979feb0
ShaySitri <sitri@post.bgu.ac.il>
2023-04-17 23:29:43 +0300

    shoot bugs fixed

--- cannon.js
+++ cannon.js
@@ -2,7 +2,7 @@ var canvas; // the canvas
 var context; // used for drawing on the canvas
 
 // constants for game play
-var TARGET_PIECES = 7; // sections in the target
+var TARGET_PIECES = 20; // sections in the target
 var MISS_PENALTY = 2; // seconds deducted on a miss
 var HIT_REWARD = 3; // seconds added on a hit
 var TIME_INTERVAL = 25; // screen refresh interval in milliseconds
@@ -18,34 +18,6 @@ var timerCount; // number of times the timer fired since the last second
 var timeLeft; // the amount of time left in seconds
 var timeElapsed; // the number of seconds elapsed
 
-// variables for the blocker and target
-var blocker; // start and end points of the blocker
-var blockerDistance; // blocker distance from left
-var blockerBeginning; // blocker distance from top
-var blockerEnd; // blocker bottom edge distance from top
-var initialBlockerVelocity; // initial blocker speed multiplier
-var blockerVelocity; // blocker speed multiplier during game
-
-var target; // start and end points of the target
-var targetDistance; // target distance from left
-var targetBeginning; // target distance from top
-var targetEnd; // target bottom's distance from top
-var pieceLength; // length of a target piece
-var initialTargetVelocity; // initial target speed multiplier
-var targetVelocity; // target speed multiplier during game
-
-var lineWidth; // width of the target and blocker
-var targetPiecesHit; // number of target pieces hit (out of 7)
-
-// variables for the cannon and cannonball
-var cannonball; // cannonball image's upper-left corner
-var cannonballVelocity; // cannonball's velocity
-var cannonballOnScreen; // is the cannonball on the screen
-var cannonballRadius; // cannonball radius
-var cannonballSpeed; // cannonball speed
-var cannonBaseRadius; // cannon base radius
-var cannonLength; // cannon barrel length
-var barrelEnd; // the end point of the cannon's barrel
 var canvasWidth; // width of the canvas
 var canvasHeight; // height of the canvas
 
@@ -98,6 +70,12 @@ var life;
 // initial start point
 var initialXpoint;
 
+// hearts
+var extaraLife = new Image();
+var extaraLifeVelocity = 150;
+var IMG_LIFE = 20;
+
+
 // var countdown =document.getElementById( "#countdown" ).countdown360({
 //    radius      : 60,
 //    seconds     : 100,
@@ -122,16 +100,6 @@ function setupGame()
    document.getElementById( "startButton" ).addEventListener( 
       "click", newGame, false );
 
-   // JavaScript Object representing game items
-   blocker = new Object(); // object representing blocker line
-   blocker.start = new Object(); // will hold x-y coords of line start
-   blocker.end = new Object(); // will hold x-y coords of line end
-   target = new Object(); // object representing target line
-   target.start = new Object(); // will hold x-y coords of line start
-   target.end = new Object(); // will hold x-y coords of line end
-   cannonball = new Object(); // object representing cannonball point
-   barrelEnd = new Object(); // object representing end of cannon barrel
-
 
    // get sounds
    targetSound = document.getElementById( "targetSound" );
@@ -142,6 +110,7 @@ function setupGame()
    enemy.src = "pic/bgud.jpg"
    enemyShoot.src = "pic/fire.jpg"
    heroShoot.src = "pic/heroShoot.jpg"
+   extaraLife.src = "pic/heart.jpg"
 
    enemyPos = new Object();
    enemyPos.start = new Object();
@@ -152,6 +121,8 @@ function setupGame()
    heroShoots = new Array();
    // initialize hitStates as an array
    hitStates = new Array(ENEMY_I);
+
+   extaraLifePos = new Object();
 
 
    keysDown = {};
@@ -183,7 +154,20 @@ function setupGame()
 
 } // end function setupGame
 
+function prizes()
+{
+   lucky_number = Math.floor(Math.random() * 2);
 
+   if (lucky_number == 7 && extaraLife.on == false)
+   {
+      extaraLife.on = true;
+      extaraLife.x = Math.floor(Math.random() * canvasWidth);
+      extaraLife.y = canvasWidth;
+   }
+
+}
+
+// delay between shoots
 function validSpacePress()
 {
    if ((now - then) < 0.75)
@@ -219,41 +203,11 @@ function resetElements()
    var h = canvas.height;
    canvasWidth = w; // store the width
    canvasHeight = h; // store the height
-   cannonBaseRadius = h / 18; // cannon base radius 1/18 canvas height
-   cannonLength = w / 8; // cannon length 1/8 canvas width
 
-   cannonballRadius = w / 36; // cannonball radius 1/36 canvas width
-   cannonballSpeed = w * 3 / 2; // cannonball speed multiplier
 
-   lineWidth = w / 24; // target and blocker 1/24 canvas width
-
-   // configure instance variables related to the blocker
-   blockerDistance = w * 5 / 8; // blocker 5/8 canvas width from left
-   blockerBeginning = h / 8; // distance from top 1/8 canvas height
-   blockerEnd = h * 3 / 8; // distance from top 3/8 canvas height
-   initialBlockerVelocity = h / 2; // initial blocker speed multiplier
-   blocker.start.x = blockerDistance;
-   blocker.start.y = blockerBeginning;
-   blocker.end.x = blockerDistance;
-   blocker.end.y = blockerEnd;
-
-   // configure instance variables related to the target
-   targetDistance = h * 7 / 8; // target 7/8 canvas width from left
-   targetBeginning = w / 8; // distance from top 1/8 canvas height
-   targetEnd = w * 7 / 8; // distance from top 7/8 canvas height
-   pieceLength = (targetEnd - targetBeginning) / TARGET_PIECES;
-   initialTargetVelocity = -h / 4; // initial target speed multiplier
-   target.start.y = targetDistance;
-   target.start.x = targetBeginning;
-   target.end.y = targetDistance;
-   target.end.x = targetEnd;
-
-   // end point of the cannon's barrel initially points horizontally
-   barrelEnd.x = cannonLength;
-   barrelEnd.y = h / 2;
-
-   enemyPos.start.x = 180;
-   enemyPos.end.x = 620;
+   // 180, 620
+   enemyPos.start.x = (800 - (ENEMY_I * ENEMY_IMG + 10 * ENEMY_J))/2;
+   enemyPos.end.x = 800 - enemyPos.start.x;
    enemyPos.start.y = canvasHeight / 25;
    enemyPos.end.y = enemyPos.start.y + 4 * ENEMY_IMG + (ENEMY_J - 1) * 10;
    enemyVelocity = 100;
@@ -265,6 +219,8 @@ function resetElements()
    heroPos.x = Math.floor(Math.random() * (800 - HERO_IMG));
    initialXpoint = heroPos.x;
    heroPos.y = canvasHeight - HERO_IMG;
+
+   extaraLife.on = false;
 
 } // end function resetElements
 
@@ -299,11 +255,8 @@ function newGame()
 
 
    targetPiecesHit = 0; // no target pieces have been hit
-   blockerVelocity = initialBlockerVelocity; // set initial velocity
-   targetVelocity = initialTargetVelocity; // set initial velocity
    timeLeft = 10; // start the countdown at 10 seconds
    timerCount = 0; // the timer has fired 0 times so far
-   cannonballOnScreen = false; // the cannonball is not on the screen
    shotsFired = 0; // set the initial number of shots fired
    timeElapsed = 0; // set the time elapsed to zero
    enemyVelocity = 150;
@@ -313,6 +266,7 @@ function newGame()
    pts = 0;
    initEnemyShoots()
    heroShoots = new Array();
+   extaraLifePos = new Object();
 	updatePositions();
    life = 3;
 
@@ -375,7 +329,6 @@ function updateHeroPos()
 function updatePositions()
 {
    document.getElementById("cords").innerHTML = life;
-
 
    updateHeroPos();
    // update the target's position
@@ -459,16 +412,19 @@ function updatePositions()
       heroShoots[i].y -= heroShootUpdate
 
       var sectionx = 
-      Math.floor((heroShoots[i].x + 0.5 * HERO_SHOOT_IMG  - enemyPos.start.x) / ENEMY_IMG);
+      Math.floor((heroShoots[i].x - enemyPos.start.x) / ENEMY_IMG);
       var sectiony = 
       Math.floor((heroShoots[i].y  - enemyPos.start.y) / ENEMY_IMG);
 
       if (heroShoots[i].y < 0)
          heroShoots.splice(i, 1)
 
+      
       if ( 
-         heroShoots[i].x + HERO_SHOOT_IMG >= enemyPos.start.x &&
-         heroShoots[i].x + HERO_SHOOT_IMG <= enemyPos.end.x && 
+         ((heroShoots[i].x + HERO_SHOOT_IMG >= enemyPos.start.x &&
+         heroShoots[i].x + HERO_SHOOT_IMG <= enemyPos.end.x) ||
+         (heroShoots[i].x >= enemyPos.start.x &&
+            heroShoots[i].x <= enemyPos.end.x)) && 
          heroShoots[i].y + HERO_SHOOT_IMG >= enemyPos.start.y &&
          heroShoots[i].y + HERO_SHOOT_IMG <= enemyPos.end.y &&
          hitStates[sectionx][sectiony] == false)
@@ -495,6 +451,21 @@ function updatePositions()
          
          timeLeft += MISS_PENALTY; // penalize the user
       } // end if
+   }
+
+   if(extaraLife.on == true)
+   {
+      extarLifeUpdate = TIME_INTERVAL / 1000.0 * heroShootVelocity;
+      extaraLifePos.y += extarLifeUpdate
+      if (heroPos.x + HERO_IMG >= extaraLifePos.x &&
+         heroPos.x + HERO_IMG <= extaraLifePos.x + IMG_LIFE && 
+         heroPos.y + HERO_IMG >= extaraLifePos.y &&
+         heroPos.y + HERO_IMG <= extaraLifePos.y + IMG_LIFE)
+         {
+            life += 1
+            extaraLife.on = false;
+
+         }
    }
 
    ++timerCount; // increment the timer event counter
@@ -581,10 +552,6 @@ function draw()
    context.textBaseline = "top";
    context.fillText("Time remaining: " + timeLeft, 5, 5);
 
-   // initialize currentPoint to the starting point of the target
-   var currentPoint = new Object();
-   currentPoint.x = target.start.x;
-   currentPoint.y = target.start.y; 
 
    
    context.drawImage(hero, heroPos.x, heroPos.y, HERO_IMG, HERO_IMG);
@@ -619,6 +586,11 @@ function draw()
    for (var i = 0; i < heroShoots.length; ++i)
    {
       context.drawImage(heroShoot,heroShoots[i].x,heroShoots[i].y, HERO_SHOOT_IMG, HERO_SHOOT_IMG);
+   }
+
+   if (extaraLife.on == true)
+   {
+      context.drawImage(IMG_LIFE,extaraLifePos.x,extaraLifePos.y, IMG_LIFE, IMG_LIFE);
    }
 
 
